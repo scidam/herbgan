e89eb2099131d782469cbb4efa97d91df084363f
Dmitry E. Kislov <kislov@easydan.com>
2019-08-24 22:47:12 +1000

    Preliminary version: testing

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ from torchvision.utils import save_image
 from torch.autograd import Variable
 
 # --------------- MAIN PARAMETERS -------------------
-BATCH_SIZE = 10
-IMAGE_SIZE = 128
+BATCH_SIZE = 2
+IMAGE_SIZE =  128
 TRAIN_IMAGES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'train_data')
 
 
@@ -80,42 +80,37 @@ imgs = imgs.numpy().transpose(0, 2, 3, 1)
 
 # --- GAN parameters ---
 INPUT_RANDOM_DIM = 128
-DECONV_KERNEL_SIZE = 4
+DECONV_KERNEL_SIZE = 2
 
 
 #
 CONV_KERNEL_SIZE = 4
-INTERMEDIATE_CONV_DIM = 756
-SMALL_CONV_DIM = 656
-LARGE_CONV_DIM = 1024
+INTERMEDIATE_CONV_DIM = 256
+SMALL_CONV_DIM = 128
+LARGE_CONV_DIM = 512
 RELU_SLOPE = 0.2
 # ----------------------
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
         self.main = nn.Sequential(
-                nn.ConvTranspose2d(INPUT_RANDOM_DIM, 512, DECONV_KERNEL_SIZE, stride=1, padding=0, bias=False),
+                nn.ConvTranspose2d(1, 512, DECONV_KERNEL_SIZE, stride=1, padding=0, bias=False),
                 nn.BatchNorm2d(512),
                 nn.ReLU(True),
-                nn.ConvTranspose2d(512, 256, DECONV_KERNEL_SIZE, stride=2, padding=1, bias=False),
+                nn.ConvTranspose2d(512, 256, DECONV_KERNEL_SIZE, stride=1, padding=1, bias=False),
                 nn.BatchNorm2d(256),
                 nn.ReLU(True),
-                nn.ConvTranspose2d(256, 128, DECONV_KERNEL_SIZE, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(128),
-                nn.ReLU(True),
-                nn.ConvTranspose2d(128, 64, DECONV_KERNEL_SIZE, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(64),
-                nn.ReLU(True),
-                nn.ConvTranspose2d(64, 3, DECONV_KERNEL_SIZE, stride=2, padding=1, bias=False),
+                nn.ConvTranspose2d(256, 3, DECONV_KERNEL_SIZE, stride=1, padding=1, bias=False),
                 nn.Tanh()
                 )
         
     def forward(self, z):
-        z = z.view(-1, INPUT_RANDOM_DIM, 1, 1)
-        img = self.model(z)
+        z = z.view(-1, 1, IMAGE_SIZE, IMAGE_SIZE)
+        img = self.main(z)
         return img
 
     
@@ -133,13 +128,16 @@ class Discriminator(nn.Module):
                 nn.Conv2d(64, 128, 8, stride=2, padding=1, bias=False),
                 nn.BatchNorm2d(128),
                 nn.LeakyReLU(negative_slope=0.2, inplace=True),
-                nn.Conv2d(128, 256, 8, stride=2, padding=1, bias=False),
+                nn.Conv2d(128, 144, 8, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(144),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                nn.Conv2d(144, 196, 8, stride=2, padding=1, bias=False),
+                nn.BatchNorm2d(196),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                nn.Conv2d(196, 256, 6, stride=2, padding=1, bias=False),
                 nn.BatchNorm2d(256),
                 nn.LeakyReLU(negative_slope=0.2, inplace=True),
-                nn.Conv2d(256, 512, 8, stride=2, padding=1, bias=False),
-                nn.BatchNorm2d(512),
-                nn.LeakyReLU(negative_slope=0.2, inplace=True),
-                nn.Conv2d(512, 1, 4, stride=1, padding=0, bias=False),
+                nn.Conv2d(256, 1, 3, stride=1, padding=1, bias=False),
                 nn.Sigmoid()
                 )
         
@@ -147,7 +145,6 @@ class Discriminator(nn.Module):
         logits = self.main(input)
         return torch.sigmoid(logits).view(-1, 1)
 
-   
     
 # Creating the discriminator
 Discriminator_net = Discriminator().to(device)
@@ -178,19 +175,20 @@ for epoch in range(NUM_EPOCHES):
         # train with real
         Discriminator_net.zero_grad()
         real_images = real_images.to(device)
+        #print("Real images: ", real_images.shape)
         batch_size = real_images.size(0)
         labels = torch.full((batch_size, 1), real_label, device=device)
-
         output = Discriminator_net(real_images)
-        print("Network output: ", output.shape)
+        
         errD_real = loss_crit(output, labels)
         errD_real.backward()
         D_x = output.mean().item()
 
         # train with fake
-        noise = torch.randn(batch_size, INPUT_RANDOM_DIM, 1, 1, device=device)
+        noise = torch.randn(batch_size, 1, IMAGE_SIZE, IMAGE_SIZE, device=device)
         fake = Generator_net(noise)
         labels.fill_(fake_label)
+
         output = Discriminator_net(fake.detach())
         errD_fake = loss_crit(output, labels)
         errD_fake.backward()
@@ -210,13 +208,15 @@ for epoch in range(NUM_EPOCHES):
         Generator_opt.step()
         
         # Save Losses for plotting later
-        G_losses.append(errG.item())
-        D_losses.append(errD.item())
-        
         if (ii+1) % (len(train_loader)//2) == 0:
             print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-                  % (epoch + 1, epochs, ii+1, len(train_loader),
+                  % (epoch + 1, NUM_EPOCHES, ii+1, len(train_loader),
                      errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-    epoch_time.append(time.time()- start)
+    # epoch_time.append(time.time()- start)
 
+noise = torch.randn(batch_size, 1, IMAGE_SIZE, IMAGE_SIZE, device=device)
+fake = Generator_net(noise)
+img = fake.detach().cpu().numpy().transpose(0, 2, 3, 1)
+plt.imshow(img[0])
+plt.show()
